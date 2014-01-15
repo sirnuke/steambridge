f0dd14e5a0828a4a6c243d5c966ba3d2b1235fa0
Bryan DeGrendel <bryan@degrendel.com>
2014-01-15 00:10:37 -0500

    1. Added autogenerated Winedump spec of Starbound's Win32 steam_api.dll (most recent Win32 release I have).

---
+++ reference/steam_api-starbound-spec/steam_api-starbound_main.c
@@ -0,0 +1,34 @@
+/*
+ * steam_api-starbound.dll
+ *
+ * Generated from steam_api-starbound.dll by winedump.
+ *
+ * DO NOT SUBMIT GENERATED DLLS FOR INCLUSION INTO WINE!
+ *
+ */
+
+#include "config.h"
+
+#include <stdarg.h>
+
+#include "windef.h"
+#include "winbase.h"
+#include "wine/debug.h"
+
+WINE_DEFAULT_DEBUG_CHANNEL(steam_api-starbound);
+
+BOOL WINAPI DllMain(HINSTANCE instance, DWORD reason, LPVOID reserved)
+{
+    TRACE("(%p, %u, %p)\n", instance, reason, reserved);
+
+    switch (reason)
+    {
+        case DLL_WINE_PREATTACH:
+            return FALSE;    /* prefer native version */
+        case DLL_PROCESS_ATTACH:
+            DisableThreadLibraryCalls(instance);
+            break;
+    }
+
+    return TRUE;
+}
