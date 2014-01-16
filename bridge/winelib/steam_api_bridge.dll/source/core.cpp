d6c20638df2e957618a05a4e4c89f1e2f44caaea
Bryan DeGrendel <bryan@degrendel.com>
2014-01-15 19:41:49 -0500

    1. Added Wine based logging stuff for the one function call.

--- bridge/winelib/steam_api_bridge.dll/source/core.cpp
+++ bridge/winelib/steam_api_bridge.dll/source/core.cpp
@@ -1,17 +1,27 @@
 // core.cpp - Handles the core API calls
 
+// C headers
 #include <cstdio>
 
+// Steam headers
 #include <steam_api.h>
 
+// Wine/Win32 headers
+#include <windef.h>
+#include <winbase.h>
+#include <wine/debug.h>
+
+// Local headers
 #include "api.h"
 
+WINE_DEFAULT_DEBUG_CHANNEL(steam_bridge);
 
 extern "C"
 {
 
 bool steam_bridge_SteamAPI_InitSafe()
 {
+  WINE_TRACE("");
   return SteamAPI_InitSafe();
 }
 
