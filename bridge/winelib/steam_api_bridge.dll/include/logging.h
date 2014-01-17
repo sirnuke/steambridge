d60df2322bedf80f6e61f87bc7d0a39d36dca0b2
Bryan DeGrendel <bryan@degrendel.com>
2014-01-17 02:50:03 -0500

    1. Copied a trimmed down version of the Proxy's logging code for our use.

---
+++ bridge/winelib/steam_api_bridge.dll/include/logging.h
@@ -0,0 +1,20 @@
+// logging.h - Handles various logging (and related) utilities.
+
+#ifndef  ___STEAM_API_BRIDGE_LOGGING_H___
+#define  ___STEAM_API_BRIDGE_LOGGING_H___
+
+#define APP_NAME "SteamAPIBridge"
+
+// FYI: Thou shall not include newlines inside of __STUB__/etc macro
+//      string arguments (if possible, as it'll screw up formatting).
+
+#define __STUB__ { __stub__(__PRETTY_FUNCTION__ , ""); }
+#define __STUB_ARGS__(ARGS, ...) { __stub__(__PRETTY_FUNCTION__ , ARGS, __VA_ARGS__); }
+void __stub__(const char *func, const char *args, ...) __attribute__ ((noreturn));
+
+#define __LOG_MSG__(MSG)  __log_message__(__PRETTY_FUNCTION__ , MSG, "")
+#define __LOG_ARGS_MSG__(MSG, ARGS, ...) __log_message__(__PRETTY_FUNCTION__ , MSG, ARGS, __VA_ARGS__)
+void __log_message__(const char *func, const char *msg, const char *args, ...);
+
+#endif //___STEAM_API_BRIDGE_LOGGING_H___
+
---
+++ bridge/winelib/steam_api_bridge.dll/source/logging.cpp
@@ -0,0 +1,38 @@
+// logging.cpp - Handles various logging (and related) utilities.
+
+#include <cstdarg>
+#include <cstdlib>
+#include <cstdio>
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+#include "logging.h"
+
+void __stub__(const char *func, const char *msg, ...)
+{
+  printf("%s ", func);
+  va_list va;
+  va_start(va, msg);
+  vprintf(msg, va);
+  printf(": is a stub\n");
+  printf("\tThis means a function is unimplemented, but is likely required\n"
+         "\tfor use in this app.  It may be possible to implement this function,\n"
+         "\tand allow this app to continue to execute.\n");
+  MessageBoxA(NULL, 
+             "Stub function called!\nCheck the command line output for more information",
+             APP_NAME " internal error",
+             MB_ICONERROR | MB_OK);
+  va_end(va);
+  exit(1);
+}
+
+void __log_message__(const char *func, const char *msg, const char *args, ...)
+{
+  printf("%s ", func);
+  va_list va;
+  va_start(va, args);
+  vprintf(args, va);
+  printf(": %s\n", msg);
+}
+
