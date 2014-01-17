f8613b5fea3b6c1121cc44f586e300407c4b7f3d
Bryan DeGrendel <bryan@degrendel.com>
2014-01-17 03:05:34 -0500

    1. Implemented a very preliminary callback class, including the wrapper.

---
+++ bridge/winelib/steam_api_bridge.dll/source/callback.cpp
@@ -0,0 +1,77 @@
+// callback.cpp - Handles callback API calls
+
+#include <cstdio>
+
+#include <steam_api.h>
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+#include <wine/debug.h>
+
+#include "api.h"
+#include "logging.h"
+
+
+WINE_DEFAULT_DEBUG_CHANNEL(steam_bridge);
+
+//class CallbackImpl : protected CCallbackBase
+// TODO: It's unclear whether making the base public (over private
+//       or protected in the steam headers) has any impact.  At the moment,
+//       it doesn't seem like it should from libsteam_api.so's perspective.
+//
+class CallbackImpl : public CCallbackBase
+{
+  public:
+    CallbackImpl(void *wrapper, int size);
+    //CCallbackBase() { m_nCallbackFlags = 0; m_iCallback = 0; }
+    // don't add a virtual destructor because we export this binary interface across dll's
+    virtual void Run(void *pvParam);
+    virtual void Run(void *pvParam, bool bIOFailure, SteamAPICall_t hSteamAPICall);
+    //int GetICallback() { return m_iCallback; }
+    virtual int GetCallbackSizeBytes();
+  private:
+    void *wrapper __attribute__ ((unused));
+    int size;
+  //protected:
+    //enum { k_ECallbackFlagsRegistered = 0x01, k_ECallbackFlagsGameServer = 0x02 };
+    //uint8 m_nCallbackFlags;
+    //int m_iCallback;
+    //friend class CCallbackMgr;
+};
+
+CallbackImpl::CallbackImpl(void *wrapper, int size) : CCallbackBase(), wrapper(wrapper), size(size)
+{
+}
+
+void CallbackImpl::Run(void *pvParam)
+  __STUB_ARGS__("(0x%p)", pvParam)
+
+void CallbackImpl::Run(void *pvParam, bool bIOFailure, SteamAPICall_t hSteamAPICall)
+  __STUB_ARGS__("(0x%p,%i,%llu)", pvParam, bIOFailure, hSteamAPICall)
+
+int CallbackImpl::GetCallbackSizeBytes()
+{
+  // TODO: It's possible this value changes in a custom implementation
+  //       of CCallbackBase, though as a wild guess this isn't used in pratice.
+  //       The two (technically three) implementations in steam_api.h return
+  //       sizeof on a class - which will be constant.
+  return size;
+}
+
+extern "C"
+{
+
+void steam_bridge_SteamAPI_RegisterCallback(void *wrapper, int callback, int size)
+{
+  // TODO: Populate CallbackImpl with data?  (flags, iCallback?)
+  WINE_TRACE("(0x%p,%i)", wrapper, callback);
+  CallbackImpl *c = new CallbackImpl(wrapper, size);
+  SteamAPI_RegisterCallback(c, callback);
+  // TODO: We should probabllly store the object.  Memory leaks and all
+  //       that.  However, it's probably a safe guess that apps rarely (if ever).
+}
+
+} // extern "C"
+
+
