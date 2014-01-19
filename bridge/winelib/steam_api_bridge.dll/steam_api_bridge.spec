c4ee8aefbf8ed1d34eaa33cfd9b66a1d34dfd4b9
Bryan DeGrendel <bryan@degrendel.com>
2014-01-19 04:02:22 -0500

    Fixed bug in the bridge spec+comment in header.
    1. What the summery sayz.

--- bridge/winelib/steam_api_bridge.dll/steam_api_bridge.spec
+++ bridge/winelib/steam_api_bridge.dll/steam_api_bridge.spec
@@ -2,3 +2,4 @@
 @ cdecl steam_bridge_SteamAPI_InitSafe()
 @ cdecl steam_bridge_SteamAPI_RegisterCallback(ptr long long)
 @ cdecl steam_bridge_SteamUser(str)
+@ cdecl steam_bridge_SteamUser_GetSteamID(ptr)
--- steam/isteamfriends.h
+++ steam/isteamfriends.h
@@ -200,7 +200,9 @@ public:
 
 	// returns true if the friend is actually in a game, and fills in pFriendGameInfo with an extra details 
 	virtual bool GetFriendGamePlayed( CSteamID steamIDFriend, FriendGameInfo_t *pFriendGameInfo ) = 0;
-	// accesses old friends names - returns an empty string when their are no more items in the history
+
+  // Lolgrammer
+	// accesses old friends names - returns an empty string when their [sic] are no more items in the history
 	virtual const char *GetFriendPersonaNameHistory( CSteamID steamIDFriend, int iPersonaName ) = 0;
 
 	// returns true if the specified user meets any of the criteria specified in iFriendFlags
