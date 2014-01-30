5c09404c8a6c071c5d5904af43c0904af6a7746a
Bryan DeGrendel <bryan@degrendel.com>
2014-01-30 14:46:16 -0500

    Updated apps_api to new logging system.

--- proxy/steam-proxy-win32/steam_api_proxy/apps_api.cpp
+++ proxy/steam-proxy-win32/steam_api_proxy/apps_api.cpp
@@ -16,16 +16,16 @@ SteamAppsWrapper::SteamAppsWrapper()
 }
 
 bool SteamAppsWrapper::BIsSubscribed()
-  __STUB__
+  __STUB("")
 
 bool SteamAppsWrapper::BIsLowViolence()
-  __STUB__
+  __STUB("")
 
 bool SteamAppsWrapper::BIsCybercafe()
-  __STUB__
+  __STUB("")
 
 bool SteamAppsWrapper::BIsVACBanned()
-  __STUB__
+  __STUB("")
 
 const char *SteamAppsWrapper::GetCurrentGameLanguage()
 {
@@ -33,7 +33,7 @@ const char *SteamAppsWrapper::GetCurrentGameLanguage()
 }
 
 const char *SteamAppsWrapper::GetAvailableGameLanguages()
-  __STUB__
+  __STUB("")
 
 bool SteamAppsWrapper::BIsSubscribedApp(AppId_t appId)
 {
@@ -41,40 +41,44 @@ bool SteamAppsWrapper::BIsSubscribedApp(AppId_t appId)
 }
 
 bool SteamAppsWrapper::BIsDlcInstalled(AppId_t appId)
-  __STUB__
+  __STUB("(%u)", appId)
 
 uint32 SteamAppsWrapper::GetEarliestPurchaseUnixTime(AppId_t nAppID)
-  __STUB__
+  __STUB("(%u)", appId)
 
 bool SteamAppsWrapper::BIsSubscribedFromFreeWeekend()
-  __STUB__
+  __STUB("")
 
 int SteamAppsWrapper::GetDLCCount()
-  __STUB__
+  __STUB("")
 
-bool SteamAppsWrapper::BGetDLCDataByIndex(int iDLC, AppId_t *pAppID, bool *pbAvailable, char *pchName, int cchNameBufferSize)
-  __STUB__
+bool SteamAppsWrapper::BGetDLCDataByIndex(int iDLC, AppId_t *pAppID,
+    bool *pbAvailable, char *pchName, int cchNameBufferSize)
+  __STUB("(%i,0x%p,0x%p,\"%s\",%i)", iDLC, pAppID, pbAvailable, pchName,
+      cchNameBufferSize)
 
 void SteamAppsWrapper::InstallDLC(AppId_t nAppID)
-  __STUB__
+  __STUB("(%u)", nAppID)
 
 void SteamAppsWrapper::UninstallDLC(AppId_t nAppID)
-  __STUB__
+  __STUB("(%u)", nAppID)
 
 void SteamAppsWrapper::RequestAppProofOfPurchaseKey(AppId_t nAppID)
-  __STUB__
+  __STUB("(%u)", nAppID)
 
 bool SteamAppsWrapper::GetCurrentBetaName(char *pchName, int cchNameBufferSize)
-  __STUB__
+  __STUB("(\"%s\",%i)", pchName, cchNameBufferSize)
 
 bool SteamAppsWrapper::MarkContentCorrupt(bool bMissingFilesOnly)
-  __STUB__
+  __STUB("(%i)", bMissingFilesOnly)
 
-uint32 SteamAppsWrapper::GetInstalledDepots(DepotId_t *pvecDepots, uint32 cMaxDepots)
-  __STUB__
+uint32 SteamAppsWrapper::GetInstalledDepots(DepotId_t *pvecDepots,
+    uint32 cMaxDepots)
+  __STUB("(0x%p,%u)", pvecDepots, cMaxDepots)
 
-uint32 SteamAppsWrapper::GetAppInstallDir(AppId_t appID, char *pchFolder, uint32 cchFolderBufferSize)
-  __STUB__
+uint32 SteamAppsWrapper::GetAppInstallDir(AppId_t appID, char *pchFolder,
+    uint32 cchFolderBufferSize)
+  __STUB("(%u,0x%p,%u)", appID, pchFolder, cchFolderBufferSize)
 
 extern "C"
 {
