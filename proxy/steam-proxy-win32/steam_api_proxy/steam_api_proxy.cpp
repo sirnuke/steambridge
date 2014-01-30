9df28e353e83376efccff6fb2cd1baa6af098103
Bryan DeGrendel <bryan@degrendel.com>
2014-01-30 15:54:56 -0500

    Logging and cleanup of core proxy files.
    1. Updated steam_api_proxy to new logging system.
    2. Initialized the global server variable to NULL for safety.
    3. Updated various comments and formatting.

--- proxy/steam-proxy-win32/steam_api_proxy/steam_api_proxy.cpp
+++ proxy/steam-proxy-win32/steam_api_proxy/steam_api_proxy.cpp
@@ -9,149 +9,162 @@ extern "C"
 {
 
 STEAM_API_PROXY_API HSteamPipe SteamAPI_GetHSteamPipe()
-  __STUB__
+  __STUB("")
 
 STEAM_API_PROXY_API HSteamUser SteamAPI_GetHSteamUser()
-  __STUB__
+  __STUB("")
 
 STEAM_API_PROXY_API const char *SteamAPI_GetSteamInstallPath()
-  __STUB__
+  __STUB("")
 
 STEAM_API_PROXY_API bool SteamAPI_IsSteamRunning()
-  __STUB__
+  __STUB("")
 
-STEAM_API_PROXY_API void SteamAPI_RegisterCallResult(void *pCallback, SteamAPICall_t hAPICall)
-  __STUB__
+STEAM_API_PROXY_API void SteamAPI_RegisterCallResult(void *pCallback,
+    SteamAPICall_t hAPICall)
+  __STUB("(0x%p,%lu)", pCallback, hAPICall)
 
 STEAM_API_PROXY_API void SteamAPI_SetBreakpadAppID(uint32 unAppID)
-  __STUB__
+  __STUB("(%u)", unAppID)
 
 STEAM_API_PROXY_API void SteamAPI_SetMiniDumpComment(const char *pchMsg)
-  __STUB__
+  __STUB("(\"%s\")", pchMsg)
 
 STEAM_API_PROXY_API void SteamAPI_SetTryCatchCallbacks(bool bTryCatchCallbacks)
-  __STUB__
+  __STUB("(%i)", pTryCatchCallbacks)
 
 STEAM_API_PROXY_API void SteamAPI_Shutdown()
-  __STUB__
+  __STUB("")
 
-STEAM_API_PROXY_API void SteamAPI_UnregisterCallResult(void *pCallback, SteamAPICall_t hAPICall)
-  __STUB__
+STEAM_API_PROXY_API void SteamAPI_UnregisterCallResult(void *pCallback,
+    SteamAPICall_t hAPICall)
+  __STUB("(0x%p,%lu)", pCallback, hAPICall)
 
 STEAM_API_PROXY_API void SteamAPI_UnregisterCallback(void *pCallback)
-  __STUB__
+  __STUB("(0x%p)", pCallback);
 
-STEAM_API_PROXY_API void SteamAPI_UseBreakpadCrashHandler(char const *pchVersion, char const *pchDate, char const *pchTime, bool bFullMemoryDumps, void *pvContext, PFNPreMinidumpCallback m_pfnPreMinidumpCallback)
-  __STUB__
+STEAM_API_PROXY_API void SteamAPI_UseBreakpadCrashHandler(
+    char const *pchVersion, char const *pchDate, char const *pchTime,
+    bool bFullMemoryDumps, void *pvContext,
+    PFNPreMinidumpCallback m_pfnPreMinidumpCallback)
+  __STUB("(\"%s\",\"%s\",\"%s\",%i,0x%p,0x%p)", pchVersion, pchDate, pchTime,
+      bFullMemoryDumps, pvContext, m_pfnPreMinidumpCallback)
 
-STEAM_API_PROXY_API void SteamAPI_WriteMiniDump(uint32 uStructuredExceptionCode, void* pvExceptionInfo, uint32 uBuildID)
-  __STUB__
+STEAM_API_PROXY_API void SteamAPI_WriteMiniDump(
+    uint32 uStructuredExceptionCode, void* pvExceptionInfo, uint32 uBuildID)
+  __STUB("(%u,0x%p,%u)", uStructuredExceptionCode, pvExceptionInfo, uBuildID)
 
 STEAM_API_PROXY_API void *SteamClient()
-  __STUB__
+  __STUB("")
 
 STEAM_API_PROXY_API void SteamContentServer()
-  __CANT_IMPLEMENT__
+  __CANT_IMPLEMENT
 STEAM_API_PROXY_API void SteamContentServerUtils()
-  __CANT_IMPLEMENT__
+  __CANT_IMPLEMENT
 STEAM_API_PROXY_API void SteamContentServer_Init()
-  __CANT_IMPLEMENT__
+  __CANT_IMPLEMENT
 STEAM_API_PROXY_API void SteamContentServer_RunCallbacks()
-  __CANT_IMPLEMENT__
+  __CANT_IMPLEMENT
 STEAM_API_PROXY_API void SteamContentServer_Shutdown()
-  __CANT_IMPLEMENT__
+  __CANT_IMPLEMENT
 STEAM_API_PROXY_API void *SteamController()
-  __CANT_IMPLEMENT__
+  __CANT_IMPLEMENT
 
 STEAM_API_PROXY_API void *SteamGameServer()
-  __STUB__
+  __STUB("")
 
 STEAM_API_PROXY_API void *SteamGameServerApps()
-  __CANT_IMPLEMENT__
+  __CANT_IMPLEMENT
 
 STEAM_API_PROXY_API void *SteamGameServerHTTP()
-  __STUB__
+  __STUB("")
 
 STEAM_API_PROXY_API void *SteamGameServerNetworking()
-  __STUB__
+  __STUB("")
 
 STEAM_API_PROXY_API void *SteamGameServerStats()
-  __STUB__
+  __STUB("")
 
 STEAM_API_PROXY_API void *SteamGameServerUtils()
-  __STUB__
+  __STUB("")
 
 STEAM_API_PROXY_API bool SteamGameServer_BSecure()
-  __STUB__
+  __STUB("")
 
 STEAM_API_PROXY_API HSteamPipe SteamGameServer_GetHSteamPipe()
-  __STUB__
+  __STUB("")
 
 STEAM_API_PROXY_API HSteamUser SteamGameServer_GetHSteamUser()
-  __STUB__
+  __STUB("")
 
 // Can probably just return 0, though it'll be easy to return the results
 // from the mothership library if we have the function definition correct.
 // Set as CANT_IMPLEMENT for now.
 STEAM_API_PROXY_API uint32 SteamGameServer_GetIPCCallCount()
-  __CANT_IMPLEMENT__
+  __CANT_IMPLEMENT
 
-// SteamIDs are a uint64 and not a typedef?  Bad Valve!
 STEAM_API_PROXY_API uint64 SteamGameServer_GetSteamID()
-  __STUB__
+  __STUB("")
 
-STEAM_API_PROXY_API bool SteamGameServer_Init(uint32 unIP, uint16 usSteamPort, uint16 usGamePort, uint16 usQueryPort, int eServerMode, const char *pchVersionString )
-  __STUB__
+STEAM_API_PROXY_API bool SteamGameServer_Init(uint32 unIP, uint16 usSteamPort,
+    uint16 usGamePort, uint16 usQueryPort, int eServerMode,
+    const char *pchVersionString)
+// TODO: uint16 is an unsigned short int, right?
+  __STUB("(%u,%hu,%hu,%hu,%i,\"%s\")", unIP, usSteamPort, usGamePort,
+      usQueryPort, eServerMode, pchVersionString)
 
-STEAM_API_PROXY_API bool SteamGameServer_InitSafe(uint32 unIP, uint16 usSteamPort, uint16 usGamePort, uint16 usQueryPort, int eServerMode, const char *pchVersionString)
-  __STUB__
+STEAM_API_PROXY_API bool SteamGameServer_InitSafe(uint32 unIP,
+    uint16 usSteamPort, uint16 usGamePort, uint16 usQueryPort, int eServerMode,
+    const char *pchVersionString)
+  __STUB("(%u,%hu,%hu,%hu,%i,\"%s\")", unIP, usSteamPort, usGamePort,
+      usQueryPort, eServerMode, pchVersionString)
 
 STEAM_API_PROXY_API void SteamGameServer_RunCallbacks()
-  __STUB__
+  __STUB("")
 
 STEAM_API_PROXY_API void SteamGameServer_Shutdown()
-  __STUB__
+  __STUB("")
 
 STEAM_API_PROXY_API void *SteamHTTP()
-  __STUB__
+  __STUB("")
 
 STEAM_API_PROXY_API void *SteamMatchmaking()
-  __STUB__
+  __STUB("")
 
 STEAM_API_PROXY_API void *SteamMatchmakingServers()
-  __STUB__
+  __STUB("")
 
 STEAM_API_PROXY_API void *SteamNetworking()
-  __STUB__
+  __STUB("")
 
 STEAM_API_PROXY_API void *SteamRemoteStorage()
-  __STUB__
+  __STUB("")
 
 STEAM_API_PROXY_API void *SteamScreenshots()
-  __STUB__
+  __STUB("")
 
 STEAM_API_PROXY_API void *SteamUGC()
-  __CANT_IMPLEMENT__
+  __CANT_IMPLEMENT
 
 STEAM_API_PROXY_API void *SteamUnifiedMessages()
-  __STUB__
+  __STUB("")
 
 STEAM_API_PROXY_API void *SteamUtils()
-  __STUB__
+  __STUB("")
 
 STEAM_API_PROXY_API HSteamUser Steam_GetHSteamUserCurrent()
-  __STUB__
+  __STUB("")
 
 // Strong suspicion that we won't be able to implement this one correctly,
 // but leave as STUB for now.
 STEAM_API_PROXY_API void Steam_RegisterInterfaceFuncs(void *hModule)
-  __STUB__
+  __STUB("(0x%p)", hModule)
 
 STEAM_API_PROXY_API void Steam_RunCallbacks(HSteamPipe hSteamPipe, bool bGameServerCallbacks)
-  __STUB__
+  __STUB("(%i,%i)", hSteamPipe, bGameServerCallbacks)
 
 // This is a variable, not a function.  Usage, however, might be ...problematic.
-STEAM_API_PROXY_API void *g_pSteamClientGameServer;
+STEAM_API_PROXY_API void *g_pSteamClientGameServer = NULL;
 
 } // extern "C"
 
--- proxy/steam-proxy-win32/steam_api_proxy/steam_api_proxy.h
+++ proxy/steam-proxy-win32/steam_api_proxy/steam_api_proxy.h
@@ -18,6 +18,7 @@ STEAM_API_PROXY_API HSteamPipe SteamAPI_GetHSteamPipe();
 // Note: *HSteamPipe are considered private wrapper functions.
 
 
+// NOTE: Probably an AppId_t
 STEAM_API_PROXY_API bool SteamAPI_RestartAppIfNecessary(uint32 unOwnAppID);
 STEAM_API_PROXY_API bool SteamAPI_IsSteamRunning();
 
@@ -32,59 +33,51 @@ STEAM_API_PROXY_API void SteamAPI_Shutdown();
 //// Callback functions:
 
 // void ... ( class CCallbackBase *pCallback, SteamAPICall_t hAPICall )
-STEAM_API_PROXY_API void SteamAPI_RegisterCallResult(void *pCallback, SteamAPICall_t hAPICall);
+STEAM_API_PROXY_API void SteamAPI_RegisterCallResult(void *pCallback,
+    SteamAPICall_t hAPICall);
 
 // void ... ( class CCallbackBase *pCallback, int iCallback )
-STEAM_API_PROXY_API void SteamAPI_RegisterCallback(class CCallbackBase *pCallback, int iCallback);
+STEAM_API_PROXY_API void SteamAPI_RegisterCallback(
+    class CCallbackBase *pCallback, int iCallback);
 
 STEAM_API_PROXY_API void SteamAPI_RunCallbacks();
 
 
 // void ... ( class CCallbackBase *pCallback, SteamAPICall_t hAPICall )
-STEAM_API_PROXY_API void SteamAPI_UnregisterCallResult(void *pCallback, SteamAPICall_t hAPICall);
+STEAM_API_PROXY_API void SteamAPI_UnregisterCallResult(
+    class CCallbackBase *pCallback, SteamAPICall_t hAPICall);
 
 // void ... ( class CCallbackBase *pCallback )
-STEAM_API_PROXY_API void SteamAPI_UnregisterCallback(void *pCallback);
+STEAM_API_PROXY_API void SteamAPI_UnregisterCallback(
+    class CCallbackBase *pCallback);
 
 
 //// Memory dump related functions?
 // Probably won't ever be correctly implemented
 
+// NOTE: unAppID is almost certainly an AppId_t, but it's an uint32 in the
+//       reference header.
 STEAM_API_PROXY_API void SteamAPI_SetBreakpadAppID(uint32 unAppID);
 STEAM_API_PROXY_API void SteamAPI_SetMiniDumpComment(const char *pchMsg);
-// void ... ( char const *pchVersion, char const *pchDate, char const *pchTime, bool bFullMemoryDumps, void *pvContext, PFNPreMinidumpCallback m_pfnPreMinidumpCallback )
-STEAM_API_PROXY_API void SteamAPI_UseBreakpadCrashHandler(char const *pchVersion, char const *pchDate, char const *pchTime, bool bFullMemoryDumps, void *pvContext, PFNPreMinidumpCallback m_pfnPreMinidumpCallback);
+// void ... ( char const *pchVersion, char const *pchDate, 
+//            char const *pchTime, bool bFullMemoryDumps, void *pvContext, 
+//            PFNPreMinidumpCallback m_pfnPreMinidumpCallback )
+STEAM_API_PROXY_API void SteamAPI_UseBreakpadCrashHandler(
+    char const *pchVersion, char const *pchDate, char const *pchTime,
+    bool bFullMemoryDumps, void *pvContext,
+    PFNPreMinidumpCallback m_pfnPreMinidumpCallback);
 
-// void ... ( uint32 uStructuredExceptionCode, void* pvExceptionInfo, uint32 uBuildID )
-STEAM_API_PROXY_API void SteamAPI_WriteMiniDump(uint32 uStructuredExceptionCode, void* pvExceptionInfo, uint32 uBuildID);
+STEAM_API_PROXY_API void SteamAPI_WriteMiniDump(
+    uint32 uStructuredExceptionCode, void *pvExceptionInfo, uint32 uBuildID);
 
 
-//// Unsorted Functions
+/// Functionality (ISteam*) getter functions:
 
 // ISteamApps *SteamApps()
 STEAM_API_PROXY_API void *SteamApps();
 
 // ISteamClient *SteamClient()
 STEAM_API_PROXY_API void *SteamClient();
-
-// All the steam content server stuff doesn't show up in the public
-// headers.  This means we have the function name, but nothing else.  Due to
-// the nature of __cdecl, nothing should blow up when this function is
-// called, but it's unlikely we can safely return from it.
-//
-// Note there is a ISteamContentServer class that is forward declared, but
-// otherwise not used.  SteamContentServer() likely returns a pointer to one.
-//
-// Theoretically, no users of steam_api.dll should use any of the
-// SteamContentServer* functions, but they are nonetheless included to
-// match the ordinal/exported functions in the steam_api.dll.
-//
-
-STEAM_API_PROXY_API void SteamContentServer();
-STEAM_API_PROXY_API void SteamContentServerUtils();
-STEAM_API_PROXY_API void SteamContentServer_Init();
-STEAM_API_PROXY_API void SteamContentServer_RunCallbacks();
-STEAM_API_PROXY_API void SteamContentServer_Shutdown();
 
 // This function does not appear in the Linux libsteam_api.so,
 // though there are a few references to SteamController in the headers.
@@ -101,6 +94,34 @@ STEAM_API_PROXY_API void *SteamController();
 
 // ISteamFriends * ... ()
 STEAM_API_PROXY_API void *SteamFriends();
+
+// ISteamHTTP * ... ()
+STEAM_API_PROXY_API void *SteamHTTP();
+// ISteamMatchmaking * ... ()
+STEAM_API_PROXY_API void *SteamMatchmaking();
+// ISteamMatchmakingServers * ... ()
+STEAM_API_PROXY_API void *SteamMatchmakingServers();
+// ISteamNetworking * ... ()
+STEAM_API_PROXY_API void *SteamNetworking();
+// ISteamRemoteStorage * ... ()
+STEAM_API_PROXY_API void *SteamRemoteStorage();
+// ISteamScreenshots * ... ()
+STEAM_API_PROXY_API void *SteamScreenshots();
+
+// This function does not appear in the public headers, nor the public
+// libsteam_api.so.  Judging by the name, it likely returns a pointer.
+STEAM_API_PROXY_API void *SteamUGC();
+
+// ISteamUnifiedMessages * ... ()
+STEAM_API_PROXY_API void *SteamUnifiedMessages();
+// ISteamUser * ... ()
+STEAM_API_PROXY_API void *SteamUser();
+// ISteamUserStats * ... ()
+STEAM_API_PROXY_API void *SteamUserStats();
+// ISteamUtils * ... ()
+STEAM_API_PROXY_API void *SteamUtils();
+
+//// SteamGameServer related functions
 
 // ISteamGameServer * ... ()
 STEAM_API_PROXY_API void *SteamGameServer();
@@ -128,55 +149,60 @@ STEAM_API_PROXY_API HSteamUser SteamGameServer_GetHSteamUser();
 STEAM_API_PROXY_API uint32 SteamGameServer_GetIPCCallCount();
 STEAM_API_PROXY_API uint64 SteamGameServer_GetSteamID();
 
-// bool ... ( uint32 unIP, uint16 usSteamPort, uint16 usGamePort, uint16 usQueryPort, EServerMode eServerMode, const char *pchVersionString )
-// TODO: enums without a type are just ints, right?  signed or
-//       unsigned? 50% sure it's just an int
-STEAM_API_PROXY_API bool SteamGameServer_Init(uint32 unIP, uint16 usSteamPort, uint16 usGamePort, uint16 usQueryPort, int eServerMode, const char *pchVersionString );
+// bool ... ( uint32 unIP, uint16 usSteamPort, uint16 usGamePort,
+//            uint16 usQueryPort, EServerMode eServerMode,
+//            const char *pchVersionString )
+STEAM_API_PROXY_API bool SteamGameServer_Init(uint32 unIP, uint16 usSteamPort,
+    uint16 usGamePort, uint16 usQueryPort, int eServerMode,
+    const char *pchVersionString);
 
-// bool ... ( uint32 unIP, uint16 usSteamPort, uint16 usGamePort, uint16 usQueryPort, EServerMode eServerMode, const char *pchVersionString )
-STEAM_API_PROXY_API bool SteamGameServer_InitSafe(uint32 unIP, uint16 usSteamPort, uint16 usGamePort, uint16 usQueryPort, int eServerMode, const char *pchVersionString);
+// bool ... ( uint32 unIP, uint16 usSteamPort, uint16 usGamePort,
+//            uint16 usQueryPort, EServerMode eServerMode,
+//            const char *pchVersionString )
+STEAM_API_PROXY_API bool SteamGameServer_InitSafe(uint32 unIP,
+    uint16 usSteamPort, uint16 usGamePort, uint16 usQueryPort, int eServerMode,
+    const char *pchVersionString);
 
 STEAM_API_PROXY_API void SteamGameServer_RunCallbacks();
 STEAM_API_PROXY_API void SteamGameServer_Shutdown();
 
-// ISteamHTTP * ... ()
-STEAM_API_PROXY_API void *SteamHTTP();
-// ISteamMatchmaking * ... ()
-STEAM_API_PROXY_API void *SteamMatchmaking();
-// ISteamMatchmakingServers * ... ()
-STEAM_API_PROXY_API void *SteamMatchmakingServers();
-// ISteamNetworking * ... ()
-STEAM_API_PROXY_API void *SteamNetworking();
-// ISteamRemoteStorage * ... ()
-STEAM_API_PROXY_API void *SteamRemoteStorage();
-// ISteamScreenshots * ... ()
-STEAM_API_PROXY_API void *SteamScreenshots();
+//// SteamContentServer related functions
 
-// This function does not appear in the public headers, nor the public
-// libsteam_api.so.  Judging by the name and location in the DLL, it likely
-// returns a pointer.
-STEAM_API_PROXY_API void *SteamUGC();
+// All the steam content server stuff doesn't show up in the public
+// headers.  This means we have the function name, but nothing else.  Due to
+// the nature of __cdecl, nothing should blow up when this function is
+// called, but it's unlikely we can safely return from it.
+//
+// Note there is a ISteamContentServer class that is forward declared, but
+// otherwise not used.  SteamContentServer() likely returns a pointer to one.
+//
+// Theoretically, no users of steam_api.dll should use any of the
+// SteamContentServer* functions, but they are nonetheless included to
+// match the exported functions in the steam_api.dll.
+//
+STEAM_API_PROXY_API void SteamContentServer();
+STEAM_API_PROXY_API void SteamContentServerUtils();
+STEAM_API_PROXY_API void SteamContentServer_Init();
+STEAM_API_PROXY_API void SteamContentServer_RunCallbacks();
+STEAM_API_PROXY_API void SteamContentServer_Shutdown();
 
-// ISteamUnifiedMessages * ... ()
-STEAM_API_PROXY_API void *SteamUnifiedMessages();
-// ISteamUser * ... ()
-STEAM_API_PROXY_API void *SteamUser();
-// ISteamUserStats * ... ()
-STEAM_API_PROXY_API void *SteamUserStats();
-// ISteamUtils * ... ()
-STEAM_API_PROXY_API void *SteamUtils();
+//// Steam 'internal' functions
 
 // Note that the following are direct interfaces to SteamClient, and
 // are considered at least semi 'internal' according to Steam documentation
 STEAM_API_PROXY_API void Steam_RegisterInterfaceFuncs(void *hModule);
-STEAM_API_PROXY_API void Steam_RunCallbacks(HSteamPipe hSteamPipe, bool bGameServerCallbacks);
+STEAM_API_PROXY_API void Steam_RunCallbacks(HSteamPipe hSteamPipe,
+    bool bGameServerCallbacks);
 STEAM_API_PROXY_API HSteamUser Steam_GetHSteamUserCurrent();
 STEAM_API_PROXY_API const char *SteamAPI_GetSteamInstallPath();
+// NOTE: Due to the nature of C++ exceptions, I doubt this works correctly
 STEAM_API_PROXY_API void SteamAPI_SetTryCatchCallbacks(bool bTryCatchCallbacks);
 
 // This is a variable, not a function.  Usage, however, might be ...problematic.
 // ISteamClient *
 STEAM_API_PROXY_API extern void *g_pSteamClientGameServer;
+
+//// Meta API calls
 
 #ifdef  ___STEAMBRIDGE_META_API___
 // This won't cause problems, right?
