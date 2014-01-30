bfc1d14396176567a7bb999a45aad33e70c92433
Bryan DeGrendel <bryan@degrendel.com>
2014-01-30 14:36:12 -0500

    Start of cleanup of Proxy logging.
    1. Changed names to be cleaner.
    2. Arguments to logging functions are no longer optional.
    3. Added TRACE.
    4. __LOG*__ is now just __LOG.
    5. Outputs to stderr, can be changed as a compile time option.

--- proxy/steam-proxy-win32/steam_api_proxy/logging.cpp
+++ proxy/steam-proxy-win32/steam_api_proxy/logging.cpp
@@ -9,66 +9,73 @@
 #include "logging.h"
 #include "types.h"
 
+#define _STREAM stderr
+#define _VA_PRINT(ARG) { \
+  va_list va; \
+  va_start(va, ARG); \
+  vfprintf(_STREAM, ARG, va); \
+  va_end(va); \
+}
+
 __declspec(noreturn) void __stub__(const char *func, const char *msg, ...)
 {
-  printf("%s ", func);
-  va_list va;
-  va_start(va, msg);
-  vprintf(msg, va);
-  printf(": is a stub\n");
-  printf("\tThis means a function is unimplemented, but is likely required\n"
-         "\tfor use in this app.  It may be possible to implement this function,\n"
-         "\tand allow this app to continue to execute.\n");
+  fprintf(_STREAM, "ERROR: %s ", func);
+  _VA_PRINT(msg);
+  fprintf(_STREAM, ": is a stub\n");
+  fprintf(_STREAM,
+      "\tThis means a function is unimplemented, but is likely required\n"
+      "\tfor use in this app.  It may be possible to implement this function,\n"
+      "\tand allow this app to continue to execute.\n");
   MessageBoxA(NULL, 
              "Stub function called!\nCheck the command line output for more information",
              APP_NAME " internal error",
              MB_ICONERROR | MB_OK);
-  va_end(va);
   exit(1);
 }
 
 __declspec(noreturn) void __cant_implement__(const char *func, const char *msg, ...)
 {
-  printf("%s ", func);
-  va_list va;
-  va_start(va, msg);
-  vprintf(msg, va);
-  printf(": cannot be implemented\n");
-  printf("\tThis means a function is unimplemented, but is likely required\n"
-         "\tfor use in this app.  However, it is not likely possible to\n"
-         "\timplement this function due to technical restrictions.\n");
+  fprintf(_STREAM, "ERROR: %s ", func);
+  _VA_PRINT(msg);
+  fprintf(_STREAM, ": cannot be implemented\n");
+  fprintf(_STREAM,
+      "\tThis means a function is unimplemented, but is likely required\n"
+      "\tfor use in this app.  However, it is not likely possible to\n"
+      "\timplement this function due to technical restrictions.\n");
   MessageBoxA(NULL, 
              "Stub function called!\nCheck the command line output for more information",
              APP_NAME " internal error",
              MB_ICONERROR | MB_OK);
-  va_end(va);
   exit(1);
+}
+
+void __trace__(const char *func, const char *args, ...)
+{
+  fprintf(_STREAM, "TRACE: %s ", func);
+  _VA_PRINT(args);
+  vprintf(_STREAM, "\n");
 }
 
 void __log_message__(const char *func, const char *msg, const char *args, ...)
 {
-  printf("%s ", func);
-  va_list va;
-  va_start(va, args);
-  vprintf(args, va);
-  printf(": %s\n", msg);
+  fprintf(_STREAM, "LOG: %s ", func);
+  _VA_PRINT(args);
+  fprintf(STREAM, ": %s\n", msg);
 }
 
 __declspec(noreturn) void __abort__(const char *func, const char *msg,
     const char *args, ...)
 {
-  printf("%s ", func);
-  va_list va;
-  va_start(va, msg);
-  vprintf(msg, va);
-  printf(": abort called\n");
-  printf("\tAn internal error of some sort has been detected, which "
-         "\tlikely signifies an internal error of some sort.");
+  fprintf(_STREAM, "ABORT: %s ", func);
+  _VA_PRINT(args);
+  fprintf(_STREAM, ": abort called\n");
+  fprintf(_STREAM,
+      "\tAn internal error of some sort has been detected, which "
+      "\tlikely signifies an internal error of some sort.");
   MessageBoxA(NULL, 
              "Abort called!\nCheck the command line output for more information",
              APP_NAME " internal error",
              MB_ICONERROR | MB_OK);
-  va_end(va);
   exit(1);
 }
 
--- proxy/steam-proxy-win32/steam_api_proxy/logging.h
+++ proxy/steam-proxy-win32/steam_api_proxy/logging.h
@@ -6,24 +6,24 @@
 // FYI: Thou shall not include newlines inside of __STUB__/etc macro
 //      string arguments (if possible, as it'll screw up formatting).
 
-#define __STUB__ { __stub__(__FUNCSIG__, ""); }
-#define __STUB_ARGS__(ARGS, ...) { __stub__(__FUNCSIG__, ARGS, __VA_ARGS__); }
+#define __STUB(ARGS, ...) { __stub__(__FUNCSIG__, ARGS, __VA_ARGS__); }
 __declspec(noreturn) void __stub__(const char *func, const char *args, ...);
 
-#define __CANT_IMPLEMENT__ { __cant_implement__(__FUNCSIG__, ""); }
-#define __CANT_IMPLEMENT_ARGS__(ARGS, ...) { __cant_implement__(__FUNCSIG__, \
-    ARGS, __VA_ARGS__); }
-__declspec(noreturn) void __cant_implement__(const char *func, const char *args, ...);
+#define __CANT_IMPLEMENT (ARGS, ...) { __cant_implement__(__FUNCSIG__, ARGS,\
+    __VA_ARGS__); }
+__declspec(noreturn) void __cant_implement__(const char *func,
+    const char *args, ...);
 
+// TODO: Compile time option to turn TRACE on/off
+#define __TRACE(ARGS, ...) __trace_message__(__FUNCSIG__, ARGS, __VA_ARGS__)
+void __trace__(const char *func, const char *args, ...);
 
-#define __LOG_MSG__(MSG)  __log_message__(__FUNCSIG__, MSG, "")
-#define __LOG_ARGS_MSG__(MSG, ARGS, ...) __log_message__(__FUNCSIG__, MSG, \
-    ARGS, __VA_ARGS__)
-void __log_message__(const char *func, const char *msg, const char *args, ...);
-
-#define __ABORT__(MSG)  __abort__(__FUNCSIG__, MSG, "")
-#define __ABORT_ARGS__(MSG, ARGS, ...)  __abort__(__FUNCSIG__, MSG, ARGS, \
+#define __LOG(MSG)  __log_message__(__FUNCSIG__, MSG, "")
+#define __LOG(MSG, ARGS, ...) __log_message__(__FUNCSIG__, MSG, ARGS, \
     __VA_ARGS__)
+void __log__(const char *func, const char *msg, const char *args, ...);
+
+#define __ABORT_(MSG, ARGS, ...)  __abort__(__FUNCSIG__, MSG, ARGS, __VA_ARGS__)
 __declspec(noreturn) void __abort__(const char *func, const char *msg,
     const char *args, ...);
 
