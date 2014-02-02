d25a3de11056707f1bec2be511bd08c93f82c860
Bryan DeGrendel <bryan@degrendel.com>
2014-02-02 04:11:15 -0500

    Added TRACE to DllMain.
    1. For Debugging purposes.

--- proxy/steam-proxy-win32/steam_api_proxy/dllmain.cpp
+++ proxy/steam-proxy-win32/steam_api_proxy/dllmain.cpp
@@ -1,11 +1,12 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "stdafx.h"
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-					 )
+#include "logging.h"
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call,
+  LPVOID lpReserved)
 {
+  __TRACE("(0x%p,%lu,0x%p])", hModule, ul_reason_for_call, lpReserved);
 	switch (ul_reason_for_call)
 	{
 	case DLL_PROCESS_ATTACH:
