0aed3a5273cbd70c5ff18ce27effebcb3771613b
Bryan DeGrendel <bryan@degrendel.com>
2014-02-05 23:59:48 -0500

    Added libconfig to Bridge linking.

--- bridge/winelib/winemaker.sh
+++ bridge/winelib/winemaker.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-winemaker steam_api_bridge.dll --nosource-fix --dll --nomfc --wine32 -Iinclude -I../../../steam/ -L../../../steam -lsteam_api -DVERSION_SAFE_STEAM_API_INTERFACES
+winemaker steam_api_bridge.dll --nosource-fix --dll --nomfc --wine32 -Iinclude -I../../../steam/ -L../../../steam -lsteam_api -DVERSION_SAFE_STEAM_API_INTERFACES -lconfig
