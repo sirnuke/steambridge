017ba1b2dd1a8004755408caab68d9b115791fa9
Bryan DeGrendel <bryan@degrendel.com>
2014-02-07 01:44:16 -0500

    Updated Proxy's meta APIs.
    1. Proxy now includes the version header from the winelib bridge, until
    this gets moved to a better location.
    2. Added a mildly useful info string for Proxy.
    3. Proxy now returns the global version.

--- proxy/steam-proxy-win32/steam_api_proxy/meta_api.cpp
+++ proxy/steam-proxy-win32/steam_api_proxy/meta_api.cpp
@@ -2,27 +2,28 @@
 
 #include "stdafx.h"
 
+
 #include "logging.h"
 #include "steam_api_proxy.h"
 
-
 #ifdef  ___STEAMBRIDGE_META_API___
 
+#include "version.h"
+
 // TODO: Real versions and info!
-const char *steambridge_version = "0";
-const char *steambridge_info = "TODO: Real information here of some sort!";
+const char *steam_bridge_PROXY_INFO = "See http://github.com/sirnuke/steambridge for more information!";
 
 extern "C"
 {
 
 STEAM_API_PROXY_API const char *Steam_proxy_version_string()
 {
-  return steambridge_version;
+  return steam_bridge_VERSION_STRING;
 }
 
 STEAM_API_PROXY_API const char *Steam_proxy_info_string()
 {
-  return steambridge_info;
+  return steam_bridge_PROXY_INFO;
 }
 
 } // extern "C"
