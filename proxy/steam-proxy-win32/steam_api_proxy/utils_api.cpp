dc51226115baa415dc6d3d534bb5c3145a8a0d3b
Bryan DeGrendel <bryan@degrendel.com>
2014-02-10 11:30:24 -0500

    Implemented stub ISteamUtils wrapper.

---
+++ proxy/steam-proxy-win32/steam_api_proxy/utils_api.cpp
@@ -0,0 +1,102 @@
+// utils_api.cpp - Utils API & SteamUtilsWrapper
+
+#include "stdafx.h"
+
+#include "logging.h"
+#include "state.h"
+#include "steam_api_proxy.h"
+#include "types.h"
+#include "utils_api.h"
+
+#include <steam_api_bridge.h>
+
+
+extern "C"
+{
+
+SteamUtilsWrapper::SteamUtilsWrapper() : steamUtils(NULL)
+{
+  __TRACE("(this=0x%p)", this);
+  // steamUtils = steam_bridge_SteamUtils();
+}
+
+uint32 SteamUtilsWrapper::GetSecondsSinceComputerActive()
+  __STUB("")
+
+int SteamUtilsWrapper::GetConnectedUniverse()
+  __STUB("")
+
+uint32 SteamUtilsWrapper::GetServerRealTime()
+  __STUB("")
+
+const char *SteamUtilsWrapper::GetIPCountry()
+  __STUB("")
+
+bool SteamUtilsWrapper::GetImageSize(int iImage, uint32 *pnWidth,
+    uint32 *pnHeight)
+  __STUB("(%i,0x%p,0x%p)", iImage, pnWidth, pnHeight)
+
+bool SteamUtilsWrapper::GetImageRGBA(int iImage, uint8 *pubDest,
+    int nDestBufferSize)
+  __STUB("(%i,0x%p,%i)", iImage, pubDest, nDestBufferSize)
+
+bool SteamUtilsWrapper::GetCSERIPPort(uint32 *unIP, uint16 *usPort)
+  __STUB("(0x%p,0x%p)", unIP, usPort)
+
+uint8 SteamUtilsWrapper::GetCurrentBatteryPower()
+  __STUB("")
+
+uint32 SteamUtilsWrapper::GetAppID()
+  __STUB("")
+
+void SteamUtilsWrapper::SetOverlayNotificationPosition(
+    int eNotificationPosition)
+  __STUB("(%i)", eNotificationPosition)
+
+bool SteamUtilsWrapper::IsAPICallCompleted(SteamAPICall_t hSteamAPICall,
+    bool *pbFailed)
+  __STUB("(%llu,0x%p)", hSteamAPICall, pbFailed)
+
+int SteamUtilsWrapper::GetAPICallFailureReason(SteamAPICall_t hSteamAPICall)
+  __STUB("(%llu)", hSteamAPICall)
+
+bool SteamUtilsWrapper::GetAPICallResult(SteamAPICall_t hSteamAPICall,
+    void *pCallback, int cubCallback, int iCallbackExpected, bool *pbFailed)
+  __STUB("(%llu,0x%p,%i,%i,0x%p)", hSteamAPICall, pCallback, cubCallback,
+      iCallbackExpected, pbFailed)
+
+void SteamUtilsWrapper::RunFrame()
+  __STUB("")
+
+uint32 SteamUtilsWrapper::GetIPCCallCount()
+  __STUB("")
+
+void SteamUtilsWrapper::SetWarningMessageHook(
+    SteamAPIWarningMessageHook_t pFunction)
+  __STUB("(0x%p)", pFunction)
+
+bool SteamUtilsWrapper::IsOverlayEnabled()
+  __STUB("")
+
+bool SteamUtilsWrapper::BOverlayNeedsPresent()
+  __STUB("")
+
+SteamAPICall_t SteamUtilsWrapper::CheckFileSignature(const char *szFileName)
+  __STUB("(\"%s\")", szFileName)
+
+bool SteamUtilsWrapper::ShowGamepadTextInput(int eInputMode, int eLineInputMode,
+    const char *pchDescription, uint32 unCharMax)
+  __STUB("(%i,%i,\"%s\",%u)", eInputMode, eLineInputMode, pchDescription,
+      unCharMax)
+
+uint32 SteamUtilsWrapper::GetEnteredGamepadTextLength()
+  __STUB("")
+
+bool SteamUtilsWrapper::GetEnteredGamepadTextInput(char *pchText, uint32 cchText)
+  __STUB("(0x%p,%u)", pchText, cchText)	
+
+const char *SteamUtilsWrapper::GetSteamUILanguage()
+  __STUB("")
+
+} //extern "C"
+
---
+++ proxy/steam-proxy-win32/steam_api_proxy/utils_api.h
@@ -0,0 +1,70 @@
+// utils_api.h - Utils API & SteamUtilsWrapper
+
+#pragma once
+
+#include <string>
+
+#include "types.h"
+
+class ISteamUtils;
+
+class SteamUtilsWrapper
+{
+  public:
+    virtual uint32 GetSecondsSinceAppActive();
+    virtual uint32 GetSecondsSinceComputerActive();
+
+    // EUniverse ... ()
+    virtual int GetConnectedUniverse();
+
+    virtual uint32 GetServerRealTime();
+
+    virtual const char *GetIPCountry();
+
+    virtual bool GetImageSize(int iImage, uint32 *pnWidth, uint32 *pnHeight);
+
+    virtual bool GetImageRGBA(int iImage, uint8 *pubDest, int nDestBufferSize);
+
+    virtual bool GetCSERIPPort(uint32 *unIP, uint16 *usPort);
+
+    virtual uint8 GetCurrentBatteryPower();
+
+    virtual uint32 GetAppID();
+
+    // void ... (ENotificationPosition)
+    virtual void SetOverlayNotificationPosition(int eNotificationPosition);
+
+    virtual bool IsAPICallCompleted(SteamAPICall_t hSteamAPICall, bool *pbFailed);
+    // ESteamAPICallFailure ... (SteamAPICall_t)
+    virtual int GetAPICallFailureReason(SteamAPICall_t hSteamAPICall);
+    virtual bool GetAPICallResult(SteamAPICall_t hSteamAPICall,
+        void *pCallback, int cubCallback, int iCallbackExpected, bool *pbFailed);
+
+    virtual void RunFrame();
+
+    virtual uint32 GetIPCCallCount();
+
+    virtual void SetWarningMessageHook(SteamAPIWarningMessageHook_t pFunction);
+
+    virtual bool IsOverlayEnabled();
+
+    virtual bool BOverlayNeedsPresent();
+
+    virtual SteamAPICall_t CheckFileSignature(const char *szFileName);
+
+    // bool ... (EGamepadTextInputMode, EGamepadTextInputLineMode,
+    //           const char *, uint32)
+    virtual bool ShowGamepadTextInput(int eInputMode, int eLineInputMode,
+        const char *pchDescription, uint32 unCharMax);
+
+    virtual uint32 GetEnteredGamepadTextLength();
+    virtual bool GetEnteredGamepadTextInput(char *pchText, uint32 cchText);	
+
+    virtual const char *GetSteamUILanguage();
+
+    SteamUtilsWrapper();
+
+  private:
+    ISteamUtils *steamUtils;
+};
+
