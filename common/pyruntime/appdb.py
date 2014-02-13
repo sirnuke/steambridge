233a67f0392a999eef6b0404a223c6e8f35dc52f
Bryan DeGrendel <bryan@degrendel.com>
2014-02-13 17:55:36 -0500

    Slightly more fleshed out AppDB Entry.

--- common/pyruntime/appdb.py
+++ common/pyruntime/appdb.py
@@ -4,18 +4,23 @@ import os
 
 import filesystem
 
-class AppDBEntry:
+class Entry:
   def __init__(self, appid):
     self.installdir = None
+    self.workingdir = None
     self._appid = appid
     self._directory = filesystem.APPDB_ROOT + str(appid)
+    self._apiversions = {}
 
     if not os.path.isdir(self._directory):
       os.mkdir(self._directory)
 
   def validate(self):
-    if self.installdir == None:
+    if self.installdir == None or self.workingdir == None:
       return False
     return True
+
+  def setapiversion(self, api, version):
+    self._apiversions[api] = version
 
 
