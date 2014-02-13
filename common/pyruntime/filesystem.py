a46698fdc2ea17ff8c0a886e3ecbcc37b10d0a2b
Bryan DeGrendel <bryan@degrendel.com>
2014-02-12 23:06:41 -0500

    PyRuntime now wraps around an appmanifest.
    1. Not a full parser, but probably sufficent.  Should write one anyways
    in the future, however.
    2. Updated the download tool to use the new PyRuntime code.

--- common/pyruntime/filesystem.py
+++ common/pyruntime/filesystem.py
@@ -1,5 +1,6 @@
 # filesystem.py - Stores various code related to the deployed filesystem
 
+import re
 import os
 
 # Root directory of the Steam installation
@@ -20,7 +21,72 @@ STEAM_API_LIB = STEAM_BRIDGE_ROOT + "/libsteam_api.so"
 # The root directory of the internal AppDB
 APPDB_ROOT = STEAM_ROOT + "/appdb"
 
+class AppManifest:
+  def __init__(self, appid):
+    if appid <= 0:
+      print "Invalid appid of {}".format(appid)
+      exit(1)
 
-def get_appmanifest_filename(appid):
-  return STEAM_ROOT + "/SteamApps/appmanifest_{}.acf".format(appid)
+    self._filename = STEAM_ROOT + "/SteamApps/appmanifest_{}.acf".format(appid)
+    self._name = None
+    self._state = None
+    self._installdir = None
+    self._appid = appid
+
+    if not os.path.isfile(self._filename):
+      self._exists = False
+    else:
+      self._exists = True
+
+  def parse(self):
+    if self._exists == False:
+      return false
+
+    with open(self._filename, 'r') as f:
+      # TODO: This is like 1/3 of the work necessary to fully parse
+      #       ACF files.  It'd be better (and less brittle) to do so
+      # TODO: A '\"' can be used in certain situations
+      keyvalue = \
+          re.compile("^\s*\"(?P<key>[^\"]+)\"\s*\"(?P<value>[^\"]+)\"\s*$")
+      for line in f:
+        r = keyvalue.search(line)
+
+        if r != None:
+          data = r.groupdict()
+
+          if data['key'] == 'AppID':
+            if int(data['value']) != appid:
+              print "ERROR: Expecting appid of {}, found {}" \
+                  .format(appid, data['value'])
+              exit(1)
+
+          elif data['key'] == 'StateFlags':
+            self._state = data['value']
+
+          elif data['key'] == 'name':
+            # The files may (might (will)) contain multiple "name" directives
+            # As it seems to be consistently the same value, this is a quick
+            # hack and to use the first one:
+            if self._name == None:
+              self._name = data['value']
+
+          elif data['key'] == 'installdir':
+            self._installdir = STEAM_ROOT \
+                + "/SteamApps/common/{}".format(data['value'])
+    return True
+
+  def exists(self):
+    return self._exists
+
+  def filename(self):
+    return self._filename
+
+  def installdir(self):
+    return self._installdir
+
+  def name(self):
+    return self._name
+
+  def appid(self):
+    return self._appid
 
--- tools/download.py
+++ tools/download.py
@@ -13,27 +13,21 @@ parser = argparse.ArgumentParser()
 parser.add_argument("appid", help="the application's id")
 # parser.add_argument("--wine", help="download through Wine steam")
 args = parser.parse_args()
-appid = args.appid
 
-if appid <= 0:
-  print "Invalid appid of {}!".format(appid)
+manifest = filesystem.AppManifest(int(args.appid))
+
+if manifest.exists():
+  print "{} already has a manifest!".format(manifest.appid())
   exit(1)
 
-filename = filesystem.get_appmanifest_filename(appid)
-
-if os.path.isfile(filename):
-  print "{} already has an appmanifest!".format(appid)
-  exit(1)
-
-f = open(filename, 'w')
-f.write('"AppState"\n')
-f.write('{\n"')
-f.write('\t"AppID"\t"{}"\n'.format(appid))
-f.write('\t"Universe"\t"1"\n')
-f.write('\t"StateFlags"\t"1026"\n')
-# StateFlags 1026 means 'Updated Started', if my sources are correct
-f.write('}\n')
-f.close()
+with open(manifest.filename(), 'w') as f:
+  f.write('"AppState"\n')
+  f.write('{\n"')
+  f.write('\t"AppID"\t"{}"\n'.format(manifest.appid()))
+  f.write('\t"Universe"\t"1"\n')
+  f.write('\t"StateFlags"\t"1026"\n')
+  # StateFlags 1026 means 'Updated Started', if my sources are correct
+  f.write('}\n')
 
 print "Done.  Restart Steam to begin the download."
 
