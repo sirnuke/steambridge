f0013c78b6c90e8fb78f833f0b72000b22d2d812
Bryan DeGrendel <bryan@degrendel.com>
2014-02-14 16:23:20 -0500

    First steps of a real execute tool.

--- tools/execute.py
+++ tools/execute.py
@@ -2,15 +2,23 @@
 
 import argparse
 
+from pyruntime import filesystem, appdb
+
 parser = argparse.ArgumentParser()
 parser.add_argument("appid", help="run this Steam game using Wine")
 args = parser.parse_args()
-appid = args.appid
 
-print "TODO: Prepare and execute ({})".format(appid)
+def err(message):
+  print message
+  exit(1)
 
-# Read internal configuration of `appid`
-# Make sure prepare has worked correctly
+appdb = appdb.Entry(str(args.appid))
+
+if appdb.exists() == False:
+  err("{} doesn't appear to be configured".format(appid))
+
+appdb.load()
+
 # Enter the 'working' directory of `appid`
 # Copy the proxy dll to steam_api.dll
 # If steam_appid.txt exists and differs in expected value, back it up
