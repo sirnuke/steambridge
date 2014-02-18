429afcb0143b23c9c2b83129773cf3291c2ec8fb
Bryan DeGrendel <bryan@degrendel.com>
2014-02-17 22:58:38 -0500

    Fixed Proxy's Apps API.

--- proxy/steam_api_proxy/apps_api.cpp
+++ proxy/steam_api_proxy/apps_api.cpp
@@ -13,86 +13,85 @@
 
 #include <steam_api_bridge.h>
 
+#define _THIN_WRAPPER(API, TRACE, ...) { \
+  __TRACE("" TRACE "", ##__VA_ARGS__); \
+  return SteamApps_  ##API(steamApps, ##__VA_ARGS__); \
+}
+
 SteamAppsWrapper::SteamAppsWrapper()
 {
   __TRACE("(this=0x%p)", this);
-  steamApps = steam_bridge_SteamApps();
+  steamApps = SteamApps_();
   __LOG("Wrapped steamApps (0x%p) in (0x%p)", steamApps, this);
 }
 
 bool SteamAppsWrapper::BIsSubscribed()
-  __STUB("")
+  _THIN_WRAPPER(BIsSubscribed, "")
 
 bool SteamAppsWrapper::BIsLowViolence()
-  __STUB("")
+  _THIN_WRAPPER(BIsLowViolence, "")
 
 bool SteamAppsWrapper::BIsCybercafe()
-  __STUB("")
+  _THIN_WRAPPER(BIsCybercafe, "")
 
 bool SteamAppsWrapper::BIsVACBanned()
-  __STUB("")
+  _THIN_WRAPPER(BIsVACBanned, "")
 
 const char *SteamAppsWrapper::GetCurrentGameLanguage()
-{
-  __TRACE("()");
-  return steam_bridge_SteamApps_GetCurrentGameLanguage(steamApps);
-}
+  _THIN_WRAPPER(GetCurrentGameLanguage, "")
 
 const char *SteamAppsWrapper::GetAvailableGameLanguages()
-  __STUB("")
+  _THIN_WRAPPER(GetAvailableGameLanguages, "")
 
 bool SteamAppsWrapper::BIsSubscribedApp(AppId_t appId)
-{
-  __TRACE("(%u)", appId);
-  return steam_bridge_SteamApps_BIsSubscribedApp(steamApps, appId);
-}
+  _THIN_WRAPPER(BIsSubscribedApp, "%u", appId)
 
 bool SteamAppsWrapper::BIsDlcInstalled(AppId_t appId)
-  __STUB("(%u)", appId)
+  _THIN_WRAPPER(BIsDlcInstalled, "%u", appId)
 
 uint32 SteamAppsWrapper::GetEarliestPurchaseUnixTime(AppId_t nAppID)
-  __STUB("(%u)", nAppID)
+  _THIN_WRAPPER(GetEarliestPurchaseUnixTime, "%u", nAppID)
 
 bool SteamAppsWrapper::BIsSubscribedFromFreeWeekend()
-  __STUB("")
+  _THIN_WRAPPER(BIsSubscribedFromFreeWeekend, "")
 
 int SteamAppsWrapper::GetDLCCount()
-  __STUB("")
+  _THIN_WRAPPER(GetDLCCount, "")
 
 bool SteamAppsWrapper::BGetDLCDataByIndex(int iDLC, AppId_t *pAppID,
     bool *pbAvailable, char *pchName, int cchNameBufferSize)
-  __STUB("(%i,0x%p,0x%p,\"%s\",%i)", iDLC, pAppID, pbAvailable, pchName,
-      cchNameBufferSize)
+  _THIN_WRAPPER(BGetDLCDataByIndex, "%i,0x%p,0x%p,\"%s\",%i", iDLC, pAppID, pbAvailable,
+      pchName, cchNameBufferSize)
 
 void SteamAppsWrapper::InstallDLC(AppId_t nAppID)
-  __STUB("(%u)", nAppID)
+  _THIN_WRAPPER(InstallDLC, "%u", nAppID)
 
 void SteamAppsWrapper::UninstallDLC(AppId_t nAppID)
-  __STUB("(%u)", nAppID)
+  _THIN_WRAPPER(UninstallDLC, "%u", nAppID)
 
 void SteamAppsWrapper::RequestAppProofOfPurchaseKey(AppId_t nAppID)
-  __STUB("(%u)", nAppID)
+  _THIN_WRAPPER(RequestAppProofOfPurchaseKey, "%u", nAppID)
 
 bool SteamAppsWrapper::GetCurrentBetaName(char *pchName, int cchNameBufferSize)
-  __STUB("(\"%s\",%i)", pchName, cchNameBufferSize)
+  _THIN_WRAPPER(GetCurrentBetaName, "\"%s\",%i", pchName, cchNameBufferSize)
 
 bool SteamAppsWrapper::MarkContentCorrupt(bool bMissingFilesOnly)
-  __STUB("(%i)", bMissingFilesOnly)
+  _THIN_WRAPPER(MarkContentCorrupt, "%i", bMissingFilesOnly)
 
 uint32 SteamAppsWrapper::GetInstalledDepots(DepotId_t *pvecDepots,
     uint32 cMaxDepots)
-  __STUB("(0x%p,%u)", pvecDepots, cMaxDepots)
+  _THIN_WRAPPER(GetInstalledDepots, "0x%p,%u", pvecDepots, cMaxDepots)
 
 uint32 SteamAppsWrapper::GetAppInstallDir(AppId_t appID, char *pchFolder,
     uint32 cchFolderBufferSize)
-  __STUB("(%u,0x%p,%u)", appID, pchFolder, cchFolderBufferSize)
+  _THIN_WRAPPER(GetAppInstallDir, "%u,0x%p,%u", appID, pchFolder, cchFolderBufferSize)
 
 extern "C"
 {
 
 STEAM_API_PROXY_API void *SteamApps()
 { 
-  __TRACE("()");
+  __TRACE("");
   return state.getSteamApps();
 }
 
