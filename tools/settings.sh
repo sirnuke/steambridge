3650daeec11d7e8745a6b7397c7a1afa82967ab1
Bryan DeGrendel <bryan@degrendel.com>
2014-04-27 02:47:34 -0400

    Various initial updates to the basic settings
    1. Better directory structure.
    2. Removed hostname in the long version string.
    3. Shell scripting is turrrrible and an awful relic from the 80s.

--- tools/settings.sh
+++ tools/settings.sh
@@ -5,7 +5,8 @@
 # as you see fit.
 
 # Root directory of the local steam installation
-STEAM_ROOT=~/.steam/root
+# (this is hardcoded to be home directory+$STEAM_ROOT)
+STEAM_ROOT=/.steam/root
 
 # Location of appmanifest_*.acf files
 STEAM_APPMANIFESTS=$STEAM_ROOT/SteamApps
@@ -13,24 +14,11 @@ STEAM_APPMANIFESTS=$STEAM_ROOT/SteamApps
 # Common root of installed Steam games
 STEAM_APPS=$STEAM_ROOT/SteamApps/common
 
-# Root directory of the SteamBridge deployment
+# Root directory of the user's SteamBridge files
 STEAM_BRIDGE_ROOT=$STEAM_ROOT/SteamBridge
 
 # Configuration filename, used by the C++ library
 CONFIG_FILENAME=$STEAM_BRIDGE_ROOT/config.json
-
-# Location of the Win32 native steam_api proxy DLL
-# This is the location when it's deployed
-PROXY_DLL=$STEAM_BRIDGE_ROOT/steam_api_proxy.dll
-
-# Location of the Winelib bridge library
-BRIDGE_LIB=$STEAM_BRIDGE_ROOT/steam_api_bridge.dll.so
-
-# Location of Winelib DLLs, used as WINEDLLPATH
-WINELIB_PATH=$STEAM_BRIDGE_ROOT
-
-# Location of the native Linux steam_api library
-STEAM_API_LIB=$STEAM_BRIDGE_ROOT/libsteam_api.so
 
 # Root directory of the internal AppDB
 APPDB_ROOT=$STEAM_BRIDGE_ROOT/appdb
@@ -38,14 +26,29 @@ APPDB_ROOT=$STEAM_BRIDGE_ROOT/appdb
 # AppDB configuration filename
 APPDB_CONFIG=appdb.json
 
+# Default prefix install directory
+PREFIX=/usr/lib/local
+
+# Shared files directory
+SHARE=$PREFIX/share/steambridge
+
+# Location of Winelib DLLs, used as WINEDLLPATH
+WINELIB_PATH=$SHARED
+#
+# Location of the Winelib bridge library
+BRIDGE_LIB=$SHARED/steam_api_bridge.dll.so
+
+# Location of the native Linux steam_api library
+STEAM_API_LIB=$SHARED/libsteam_api.so
+
+# Location of the Win32 native steam_api proxy DLL
+PROXY_DLL=$SHARED/steam_api_proxy.dll
+
 # Directory storing the SteamBridge tools
-TOOLS_DIR=$STEAM_BRIDGE_ROOT/bin
+TOOLS_DIR=$PREFIX/bin
 
-# Directory storing the Python Runtime library
-PYRUNTIME_DIR=$TOOLS_DIR/pyruntime
-
-# Location of the execute tool script
-EXECUTE_TOOL=$TOOLS_DIR/execute.py
+# Directory storing the Python support module 
+PYRUNTIME_DIR=$SHARE/pysteambridge
 
 if [ -f common/local.sh ]; then
   . ./common/local.sh
@@ -65,8 +68,7 @@ VERSION_PATCH=2
 # Bit of data about the build
 GIT_HEAD=`git rev-parse HEAD`
 CONFIG_TIME=`date`
-MACHINE_HOSTNAME=`hostname|sed -e 's/[ ]*$//'`
 
 VERSION_SHORT="$VERSION_MAJOR.$VERSION_MINOR.$VERSION_PATCH"
-VERSION_LONG="$VERSION_MAJOR.$VERSION_MINOR.$VERSION_PATCH (git-head:$GIT_HEAD, config-time:$CONFIG_TIME, hostname:$MACHINE_HOSTNAME)"
+VERSION_LONG="$VERSION_MAJOR.$VERSION_MINOR.$VERSION_PATCH (git-head:$GIT_HEAD, config-time:$CONFIG_TIME)"
 
