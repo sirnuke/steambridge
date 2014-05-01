4a60f578d3f6d2f383a0a8ceac34baeeb74b2ea8
Bryan DeGrendel <bryan@degrendel.com>
2014-05-01 14:50:58 -0400

    Reimplemented the download action

--- tools/pysteambridge/download.py
+++ tools/pysteambridge/download.py
@@ -1,32 +1,31 @@
 #!/usr/bin/env python2.7
 
-import argparse
-
-from pyruntime import appmanifest
+import appmanifest
 
 # TODO: Confirm the game is owned by steam?
 # TODO: Option to clean install and redownload?
 # TODO: Option to download through Wine Steam?
 
-parser = argparse.ArgumentParser()
-parser.add_argument("appid", help="the application's id")
-# parser.add_argument("--wine", help="download through Wine steam")
-args = parser.parse_args()
+class DownloadException:
+  def __init__(self, message):
+    self._message = message
 
-manifest = appmanifest.AppManifest(int(args.appid))
+  def __str__(self):
+    return self._message
 
-if manifest.exists():
-  print "{} already has a manifest!".format(manifest.appid())
-  exit(1)
+# appid must be an int
+def do(appid):
+  manifest = appmanifest.AppManifest(appid)
 
-with open(manifest.filename(), 'w') as f:
-  f.write('"AppState"\n')
-  f.write('{\n"')
-  f.write('\t"AppID"\t"{}"\n'.format(manifest.appid()))
-  f.write('\t"Universe"\t"1"\n')
-  f.write('\t"StateFlags"\t"1026"\n')
-  # StateFlags 1026 means 'Updated Started', if my sources are correct
-  f.write('}\n')
+  if manifest.exists():
+    raise DownloadException("{} already has a manifest!".format(appid))
 
-print "Done.  Restart Steam to begin the download."
+  with open(manifest.filename(), 'w') as f:
+    f.write('"AppState"\n')
+    f.write('{\n"')
+    f.write('\t"AppID"\t"{}"\n'.format(manifest.appid()))
+    f.write('\t"Universe"\t"1"\n')
+    f.write('\t"StateFlags"\t"1026"\n')
+    # StateFlags 1026 means 'Updated Started', if my sources are correct
+    f.write('}\n')
 
