915deea74dfd018e8b14026337c8dac319a828d8
Bryan DeGrendel <bryan@degrendel.com>
2014-05-04 00:17:51 -0400

    Reimplemented the execute action

--- tools/pysteambridge/execute.py
+++ tools/pysteambridge/execute.py
@@ -1,45 +1,44 @@
 #!/usr/bin/env python2.7
 
-import argparse
-import shutil
-import os
+import os, shutil
 
-from pyruntime import config, filesystem, appdb
+import config, filesystem, appdb
 
-parser = argparse.ArgumentParser()
-parser.add_argument("appid", help="run this Steam game using Wine")
-args = parser.parse_args()
+class ExecuteException(Exception):
+  def __init__(self, message):
+    self._message = message
 
-def err(message):
-  print message
-  exit(1)
+  def __str__(self):
+    return self._message
 
-appdb = appdb.Entry(str(args.appid))
+# appid must be an integer
+def do(appid):
+  app = appdb.Entry(appid)
+  if not app.exists():
+    raise ExecuteException("{} isn't configured (steambridge configure {})".format(appid, appid))
 
-if appdb.exists() == False:
-  err("{} doesn't appear to be configured".format(appdb.appid()))
+  app.load()
 
-appdb.load()
+  if not app.validate():
+    raise ExecuteException("{} has an invalid internal configuration, try rerunning configure" \
+        .format(appid))
 
-if not appdb.validate():
-  err("{} has an invalid appdb file".format(appdb.appid()))
+  # Copy the proxy DLL
+  shutil.copyfile(config.PROXY_DLL, app.workingdir + "/steam_api.dll")
 
-# Copy the proxy DLL
-shutil.copyfile(config.PROXY_DLL, appdb.workingdir + "/steam_api.dll")
+  # Set appid.txt, if it doesn't already exist
+  # TODO: Check to make sure the existing appid.txt has the right value?
+  if not os.path.isfile(app.workingdir + "/steam_appid.txt"):
+    with open(app.workingdir + "/steam_appid.txt", 'w') as f:
+      f.write(app.appid())
 
-# Set appid.txt, if it doesn't already exist
-# TODO: Check to make sure the existing appid.txt has the right value?
-if not os.path.isfile(appdb.workingdir + "/steam_appid.txt"):
-  with open(appdb.workingdir + "/steam_appid.txt", 'w') as f:
-    f.write(appdb.appid())
+  # Change to the installation directory and execute using wine
+  os.chdir(app.installdir)
+  # TODO: wineprefix
+  cmd = 'WINEDEBUG="+steambridge" WINEDLLPATH="{}" wine "{}" 2>"{}/stderr.txt" >"{}/stdout.txt"' \
+      .format(config.WINELIB_PATH, app.executable, appdb.directory(), appdb.directory())
 
-# Change to the installation directory and execute using wine
-os.chdir(appdb.installdir)
-# TODO: wineprefix
-cmd = 'WINEDEBUG="+steambridge" WINEDLLPATH="{}" wine "{}" 2>"{}/stderr.txt" >"{}/stdout.txt"' \
-    .format(config.WINELIB_PATH, appdb.executable, appdb.directory(), appdb.directory())
+  print "DEBUG: execute('{}')".format(cmd)
 
-print "DEBUG: execute('{}')".format(cmd)
-
-filesystem.execute(cmd, ignore_results=True)
+  filesystem.execute(cmd, ignore_results=True)
 
--- tools/steambridge.py
+++ tools/steambridge.py
@@ -13,7 +13,7 @@ def error(message):
 
 try:
   from pysteambridge import config, filesystem
-  from pysteambridge import download
+  from pysteambridge import download, execute
   APP_NAME = config.APP_NAME
 except filesystem.FilesystemException, e:
   error(e)
@@ -30,10 +30,15 @@ def help():
 class CLI:
   def download(self, args):
     parser = argparse.ArgumentParser(prog='{} download'.format(config.APP_NAME))
-    #parser.add_argument('appid', type=int, required=True, help='the AppID that will be downloaded')
     parser.add_argument('appid', type=int, help='the AppID that will be downloaded')
     args = parser.parse_args(args)
     download.do(args.appid)
+
+  def execute(self, args):
+    parser = argparse.ArgumentParser(prog='{} execute'.format(config.APP_NAME))
+    parser.add_argument('appid', type=int, help='the AppID to execute')
+    args = parser.parse_args(args)
+    execute.do(args.appid)
 
 if len(sys.argv) < 2:
   # TODO: This will mean GUI, in the future
@@ -46,6 +51,8 @@ if cmd == '--help' or cmd == '-h':
   help()
 elif cmd == 'download':
   cli.download(sys.argv[2:])
+elif cmd == 'execute':
+  cli.execute(sys.argv[2:])
 else:
   if len(cmd) >= 2 and cmd[0:2] == '--':
     error("Unexpected flag '{}', expected --help or command".format(sys.argv[1]))
