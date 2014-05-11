eee643a4d728e8cb58246c9e9a4767020e1631a6
Bryan DeGrendel <bryan@degrendel.com>
2014-05-10 23:50:15 -0400

    Updated the winemaker script

--- buildhelper/winemaker.sh
+++ buildhelper/winemaker.sh
@@ -3,6 +3,7 @@ LIBRARIES="-ldl"
 INCLUDEPATH="-I../../common/steam/ -I../../common/include/"
 LIBRARYPATH=""
 DEFINES="-DVERSION_SAFE_STEAM_API_INTERFACES"
+cd libraries/bridge
 winemaker steam_api_bridge.dll --nosource-fix --dll --nomfc --wine32 $DEFINES $INCLUDEPATH $LIBRARYPATH $LIBRARIES
 # There HAS to be a better way to do this
 sed -i 's/-m32/-m32 -Wall -Wextra -pipe -g/g' steam_api_bridge.dll/Makefile
