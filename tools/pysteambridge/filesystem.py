6db3e36250448227b94dece7b07a11f16153000f
Bryan DeGrendel <bryan@degrendel.com>
2015-05-22 23:16:49 -0400

    Added a user friendly hint to Filesytem exceptions

--- tools/pysteambridge/filesystem.py
+++ tools/pysteambridge/filesystem.py
@@ -11,22 +11,23 @@ _MAKE_DIRS = [config.STEAM_APPMANIFESTS, config.STEAM_APPS, config.STEAM_BRIDGE_
 _BRIDGE_FILES = [config.PROXY_DLL, config.BRIDGE_LIB, config.STEAM_API_LIB]
 
 class FilesystemException(Exception):
-  def __init__(self, type, path):
+  def __init__(self, type, path, hint):
     self._type = type
     self._path = path
+    self._hint = hint
 
   def __str__(self):
-    return "{} may not be corrupt because '{}' is missing" \
-        .format(self._type, self._path)
+    return "{} may be corrupt; '{}' not found ({})" \
+        .format(self._type, self._path, self._hint)
 
 def validate():
   for i in _STEAM_DIRS:
     if not os.path.isdir(i):
-      raise FilesystemException('Steam', i)
+      raise FilesystemException('Steam', i, "Is Linux Steam properly installed?")
 
   for i in _BRIDGE_FILES:
     if not os.path.isfile(i):
-      raise FilesystemException(config.APP_NAME, i)
+      raise FilesystemException(config.APP_NAME, i, "Is SteamBridge properly installed?")
 
   for i in _MAKE_DIRS:
     if not os.path.isdir(i):
